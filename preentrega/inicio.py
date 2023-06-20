03d486883d5984a697832cc413c50eecff0ecae3
Emmy Tamayo <eigram1626@gmail.com>
2023-06-21 01:04:52 +0200

    "bug"

--- preentrega/inicio.py
+++ preentrega/inicio.py
@@ -1,17 +1,20 @@
-{"nombre": str,
- "password": str,}
-
 import json
+import pathlib
 
+Base_Dir = pathlib.Path(__file__).resolve().parent
+Ruta_Base_datos = Base_Dir / "base_usuarios.json"
+base_datos = []
 def entrada_datos():
     usuarios = {}
     nombre = input("Nombre: ")
     password = input("Contraseña: ")
+    base_datos.append(usuarios)
     usuarios[nombre] = password
-    return usuarios
+    guardar_datos()
 
 def guardar_datos():
-    ...
+    with open(Ruta_Base_datos, "w") as archivo:
+        json.dump(base_datos, archivo, indent=4)
 
 def mostrar_datos():
     ...
